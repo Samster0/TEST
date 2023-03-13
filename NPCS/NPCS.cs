8f7364291c165440e6431d78074181bad29eff6f
Samster0 <Sjurcazak@gmail.com>
2023-03-13 12:21:15 -0700

    Update NPCS.cs

--- NPCS/NPCS.cs
+++ NPCS/NPCS.cs
@@ -25,8 +25,12 @@ namespace TEST.NPCS
             NPC.aiStyle = 51;
             NPC.HitSound = SoundID.NPCHit1;
             NPC.DeathSound = SoundID.NPCDeath1;
-            AIType = NPCID.Plantera;
-            AnimationType = NPCID.Plantera;
+            NPC.boss = true;
+            NPC.noGravity = true;
+          
+
+            
+
         }
         public override float SpawnChance(NPCSpawnInfo spawnInfo)
         {
@@ -36,7 +40,7 @@ namespace TEST.NPCS
         public override void FindFrame(int frameHeight)
         {
             NPC.frameCounter++;
-            if (NPC.frameCounter >= 20)
+            if (NPC.frameCounter >= 20) 
             {
                 NPC.frameCounter = 0;
             }
@@ -48,6 +52,42 @@ namespace TEST.NPCS
             Item.NewItem(NPC.GetSource_Death(), NPC.getRect(), ItemID.VortexBeater, Main.rand.Next(0, 2));
             Item.NewItem(NPC.GetSource_Death(), NPC.getRect(), ItemID.TempleKey, Main.rand.Next(5, 10));
         }
+
+        public override void AI()
+        {
+            if (NPC.life > NPC.lifeMax / 2)
+            {
+                // boss attacks when its health is above 50%
+               
+                if (NPC.frameCounter % 60 == 0)
+                {
+                    // boss does an attack every second
+                    Player target = Main.player[NPC.target];
+                    Vector2 direction = target.Center - NPC.Center;
+                    direction.Normalize();
+                    int projectile = Projectile.NewProjectile(NPC.GetSource_FromThis(), NPC.Center, direction * 10f, ProjectileID.Fireball, NPC.damage, 0f);
+                  
+                }
+            }
+            else
+            {
+                // boss attacks more aggressively when its health is below 50%
+                NPC.velocity = new Vector2(4f, 0f);
+                if (NPC.frameCounter % 30 == 0)
+                {
+                    // boss does an attack every half second
+                    Player target = Main.player[NPC.target];
+                    Vector2 direction = target.Center - NPC.Center;
+                    direction.Normalize();
+                    int projectile = Projectile.NewProjectile(NPC.GetSource_FromThis(), NPC.Center, direction * 10f, ProjectileID.Fireball, NPC.damage, 0f);
+                }
+            }
+        }
+
+
+
+
+
 
     }
 }
